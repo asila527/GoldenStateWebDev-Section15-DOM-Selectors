a70842f80275b1d1e49f25d73b82534a560b702d
Ellie S <asila527@gmail.com>
2020-10-15 16:24:40 -0700

    add files

---
+++ selectors.js
@@ -0,0 +1,7 @@
+//4 different ways to select the first paragraph
+
+document.getElementById('first');
+document.querySelector('#first');
+document.querySelector('.special');
+document.querySelector('p');
+document.getElementsByTagName('p')[0];
